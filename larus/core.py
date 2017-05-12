816b9df6c98ec2078359a28a00cb6974beaa6c5a
Roberto De Almeida <roberto@dealmeida.net>
2017-05-11 22:38:55 -0700

    Initial code, learning how to use jack

--- larus/core.py
+++ larus/core.py
@@ -1,1 +1,44 @@
 # -*- coding: utf-8 -*-
+
+import threading
+
+import jack
+import numpy as np
+
+from mixer import Mixer
+
+
+client = jack.Client("Larus")
+event = threading.Event()
+
+
+@client.set_process_callback
+def process(frames):
+    assert frames == client.blocksize
+    for i, o in zip(client.inports, client.outports):
+        print(np.average(i.get_array()))
+        # o.get_buffer()[:] = i.get_buffer()
+
+
+@client.set_shutdown_callback
+def shutdown(status, reason):
+    print('JACK shutdown!')
+    print('status:', status)
+    print('reason:', reason)
+    event.set()
+
+
+def main():
+    Mixer(client)
+    client.activate()
+
+    with client:
+        print('Press Ctrl+C to stop')
+        try:
+            event.wait()
+        except KeyboardInterrupt:
+            print('\nInterrupted by user')
+
+
+if __name__ == '__main__':
+    main()
--- larus/mixer.py
+++ larus/mixer.py
@@ -1,1 +1,37 @@
 # -*- coding: utf-8 -*-
+
+"""
+# Larus mixer #
+
+This is a 8-track mixer that displays levels in the Launchpad Mini, and whose
+individual levels can be controlled from there.
+
+"""
+
+
+class Mixer:
+
+    def __init__(self, client):
+        self.client = client
+        self.register_ports()
+
+    def register_ports(self):
+        """
+        Register 8 input tracks and 2 output tracks.
+
+        """
+        self.inports = [
+            self.client.inports.register(
+                'input_{track}_{channel}'.format(
+                    track=track, channel=channel))
+            for track in (1, 2, 3, 4)
+            for channel in (1, 2)
+        ]
+        self.outports = [
+            self.client.outports.register('output_{channel}'.format(
+                channel=channel))
+            for channel in (1, 2)
+        ]
+
+    def process(self, frames):
+        pass
